e2dae05a431e5532571e58fdb0fdece47c932867
AnnabelleYChan <chana8@rpi.edu>
2024-04-02 16:58:23 -0400

    Added a javascript file for tutorials and added a method to ui_tutorials.py to create the gui component

--- modules/ui_tutorials.py
+++ modules/ui_tutorials.py
@@ -19,3 +19,39 @@ def get_value_for_setting(key):
 
     return gr.update(value=value, **args)
 
+# Creates a GUI component for a tutorial specified by the key 
+def create_tutorials_component(key, is_quicktutorials=False):
+    def fun():
+        return opts.data[key] if key in opts.data else opts.data_labels[key].default
+
+    info = opts.data_labels[key]
+    t = type(info.default)
+
+    args = info.component_args() if callable(info.component_args) else info.component_args
+
+    if info.component is not None:
+        comp = info.component
+    elif t == str:
+        comp = gr.Textbox
+    elif t == int:
+        comp = gr.Number
+    elif t == bool:
+        comp = gr.Checkbox
+    else:
+        raise Exception(f'bad options item type: {t} for key {key}')
+
+    elem_id = f"tutorials_{key}"
+
+    if info.refresh is not None:
+        if is_quicktutorials:
+            res = comp(label=info.label, value=fun(), elem_id=elem_id, **(args or {}))
+            ui_common.create_refresh_button(res, info.refresh, info.component_args, f"refresh_{key}")
+        else:
+            with FormRow():
+                res = comp(label=info.label, value=fun(), elem_id=elem_id, **(args or {}))
+                ui_common.create_refresh_button(res, info.refresh, info.component_args, f"refresh_{key}")
+    else:
+        res = comp(label=info.label, value=fun(), elem_id=elem_id, **(args or {}))
+
+    return res
+
